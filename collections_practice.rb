b4b5260dde4199ead892f33d342339ebf95be883
Shea Kusiak <shkusiak@gmail.com>
2018-02-21 01:29:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array_second_element = array[1]
+  array_third_element = array[2]
+  array[1] = array_third_element
+  array[2] = array_second_element
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  #array.collect
+  #array.each do |
+end
+
+def find_a(array)
+  array.find_all {|word|
+    word.chars.first == "a"
+  }
+end
+
+def sum_array(array)
+  array.inject { |sum, n|
+  sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index|
+    if element == "feet"
+      element
+    else
+      element + "s"
+    end
+  }
+end
